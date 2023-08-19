6913220b97561a9fbbdb5b8ac15cc803b5bf7d00
Craig <craig.branscom@gmail.com>
2023-08-19 19:34:26 -0400

    importer cleanup

--- engine/importer/importer.go
+++ engine/importer/importer.go
@@ -17,45 +17,19 @@ func BuildNodeFromYaml(data map[interface{}]interface{}) nodes.Node {
 		return node
 	case "Node2D":
 		baseNode := nodes.BuildBaseNode(data["name"].(string), nil)
-		xPos, xErr := strconv.ParseFloat(data["xPos"].(string), 64)
-		if xErr != nil {
-			log.Fatalf("Error parsing x value: %v", xErr)
-		}
-		yPos, yErr := strconv.ParseFloat(data["yPos"].(string), 64)
-		if yErr != nil {
-			log.Fatalf("Error parsing y value: %v", yErr)
-		}
-		pos2DComponent := components.BuildPosition2DComponent(xPos, yPos)
+		pos2DComponent := BuildPosition2DComponentFromYaml(data)
 		node := nodes.BuildNode2D(*baseNode, *pos2DComponent)
 		BuildNodeChildrenFromYaml(node, data)
 		return node
 	case "Line2D":
 		baseNode := nodes.BuildBaseNode(data["name"].(string), nil)
-		xPos, xErr := strconv.ParseFloat(data["xPos"].(string), 64)
-		if xErr != nil {
-			log.Fatalf("Error parsing x value: %v", xErr)
-		}
-		yPos, yErr := strconv.ParseFloat(data["yPos"].(string), 64)
-		if yErr != nil {
-			log.Fatalf("Error parsing y value: %v", yErr)
-		}
-		pos2DComponent := components.BuildPosition2DComponent(xPos, yPos)
+		pos2DComponent := BuildPosition2DComponentFromYaml(data)
 		node2d := nodes.BuildNode2D(*baseNode, *pos2DComponent)
-
 		pointsIfaceSlice := data["points"].([]interface{})
 		var points []components.Position2D
 		for _, pointIface := range pointsIfaceSlice {
-			m := pointIface.(map[interface{}]interface{})
-			x, xErr := strconv.ParseFloat(m["xPos"].(string), 64)
-			if xErr != nil {
-				log.Fatalf("Error parsing x value: %v", xErr)
-			}
-			y, yErr := strconv.ParseFloat(m["yPos"].(string), 64)
-			if yErr != nil {
-				log.Fatalf("Error parsing y value: %v", yErr)
-			}
-			pos := components.BuildPosition2DComponent(x, y)
-			points = append(points, *pos)
+			point := BuildPosition2DComponentFromYaml(pointIface.(map[interface{}]interface{}))
+			points = append(points, *point)
 		}
 		width, widthErr := strconv.ParseUint(data["width"].(string), 10, 32)
 		if widthErr != nil {
@@ -76,4 +50,16 @@ func BuildNodeChildrenFromYaml(node nodes.Node, data map[interface{}]interface{}
 		child := BuildNodeFromYaml(childData.(map[interface{}]interface{}))
 		node.AppendChild(child)
 	}
+}
+
+func BuildPosition2DComponentFromYaml(data map[interface{}]interface{}) *components.Position2D {
+	xPos, xErr := strconv.ParseFloat(data["xPos"].(string), 64)
+	if xErr != nil {
+		log.Fatalf("Error parsing x value: %v", xErr)
+	}
+	yPos, yErr := strconv.ParseFloat(data["yPos"].(string), 64)
+	if yErr != nil {
+		log.Fatalf("Error parsing y value: %v", yErr)
+	}
+	return components.BuildPosition2DComponent(xPos, yPos)
 }
